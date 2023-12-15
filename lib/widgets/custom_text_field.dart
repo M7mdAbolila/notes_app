99f5ebb6a07411310eae05941fd4b6a2ac625d1b
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2023-12-15 16:59:33 +0200

    add fields to bottom sheet

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hintText, this.maxLines = 1});
+  final String hintText;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxLines,
       cursorColor: kprimaryColor,
       decoration: InputDecoration(
-        hintText: 'title',
+        hintText: hintText,
         hintStyle: const TextStyle(color: kprimaryColor),
         border: buildBorder(),
         enabledBorder: buildBorder(),
